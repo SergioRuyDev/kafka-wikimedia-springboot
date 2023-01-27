a14e6c81cee7f63dcab2ab24c39d1560f71424cb
sergioruydev <sergioruyenator@gmail.com>
2023-01-27 13:45:41 -0300

    Producer start.

--- kafka-producer-wikimedia/src/main/java/com/sergioruy/SpringBootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/com/sergioruy/SpringBootProducerApplication.java
@@ -1,11 +1,23 @@
 package com.sergioruy;
 
+import com.sergioruy.service.WikimediaChangesProducer;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@AllArgsConstructor
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication implements CommandLineRunner {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class);
+    }
+
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
