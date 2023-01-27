f6979a1104cc72967dbb34a74ee760cf84ac633f
sergioruydev <sergioruyenator@gmail.com>
2023-01-27 12:11:45 -0300

    Initial commit

---
+++ src/main/java/com/sergioruy/kafkawikimediaspringboot/KafkaWikimediaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sergioruy.kafkawikimediaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWikimediaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWikimediaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergioruy/kafkawikimediaspringboot/KafkaWikimediaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergioruy.kafkawikimediaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWikimediaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
